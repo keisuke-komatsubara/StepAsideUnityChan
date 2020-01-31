90e56b6a07c3f36f80e9fb2db31faedf669011a0
小松原桂介 <kk.kabocha@outlook.com>
2020-02-01 00:24:07 +0900

    [add]lesson6 課題、発展課題
    画面外に出たオブジェクトが破棄されるようにしました。
    Unityちゃんの50m先までオブジェクトがリアルタイムで生成されるようにしました。

---
+++ Assets/ObjectController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour {
+    //カメラを入れる
+    public GameObject MainCamObj;
+    //ゴール地点
+    private int goalPos = 120;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //カメラを取得
+        MainCamObj = Camera.main.gameObject;
+
+        if (transform.position.z - MainCamObj.transform.position.z < 0)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z > this.goalPos)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
